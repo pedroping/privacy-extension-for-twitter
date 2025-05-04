57ea5f02fdfbd67f12f6b3868a26af9c5cd32aad
pedroping <pedro.dias@moneyprestador.com.br>
2025-05-04 20:11:18 -0300

    Fix: create page content mutation observers

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -2,18 +2,77 @@ if (typeof browser == "undefined") {
   globalThis.browser = chrome;
 }
 
-function updateStyles() {
+function updateData() {
   browser.storage.sync.get([settingsIdentifier]).then((result) => {
-    console.log("Result", result);
-
     browser.storage.sync.set(result);
+
+    try {
+      const data = JSON.parse(result.data);
+
+      if (!data) return;
+
+      blurContent(data);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+}
+
+async function blurContent(data) {
+  const main = document.body.querySelector("main");
+  const contentSection = await getSectionContent();
+
+  observeDOM(contentSection, (a) => {
+    console.log(contentSection.children);
+  });
+}
+
+function getSectionContent() {
+  return new Promise((resolve) => {
+    let contentSection = document.body.querySelector(
+      '[aria-labelledby="accessible-list-0"]'
+    );
+
+    if (contentSection) {
+      resolve(contentSection);
+    }
+
+    const interval = setInterval(() => {
+      contentSection = document.body.querySelector(
+        '[aria-labelledby="accessible-list-0"]'
+      );
+
+      if (contentSection) {
+        clearInterval(interval);
+        resolve(contentSection);
+      }
+    }, 1000);
   });
 }
 
 const settingsIdentifier = "data";
 
 browser.storage.onChanged.addListener(() => {
-  updateStyles();
+  updateData();
 });
 
-updateStyles();
+updateData();
+
+function observeDOM(obj, callback) {
+  const MutationObserver =
+    window.MutationObserver || window.WebKitMutationObserver;
+
+  if (!obj || obj.nodeType !== 1) {
+    return;
+  }
+
+  if (MutationObserver) {
+    const mutationObserver = new MutationObserver(callback);
+
+    mutationObserver.observe(obj, { childList: true, subtree: true });
+    return mutationObserver;
+  } else if (window.addEventListener) {
+    obj.addEventListener("DOMNodeInserted", callback, false);
+    obj.addEventListener("DOMNodeRemoved", callback, false);
+  }
+}
