445201f9ad5bda036d37a0ff6b3c4e5cbcebadc4
pedroping <pedro.dias@moneyprestador.com.br>
2025-05-24 17:28:29 -0300

    Feat: create custom opacity filter popup

--- content/index.js
+++ content/index.js
@@ -13,13 +13,18 @@ let intialData = {
   blurTextInputs: false,
 };
 
+const actionPopup = document.getElementById("action-popup");
+
 Array.from(document.querySelectorAll('input[type="checkbox"]')).forEach(
   (input) => {
     input.addEventListener("change", (event) => {
       const key = event.target.getAttribute("eventId");
-      const value = event.target.checked;
+      const value = !!event.target.checked;
 
       intialData[key] = value;
+
+      const button = document.querySelector(`button[eventId="${key}"]`);
+      if (button) button.disabled = !value;
 
       browser.storage.sync.set({
         data: JSON.stringify(intialData),
@@ -27,6 +32,29 @@ Array.from(document.querySelectorAll('input[type="checkbox"]')).forEach(
     });
   }
 );
+
+Array.from(document.querySelectorAll("button")).forEach((button) => {
+  button.addEventListener("click", (event) => {
+    event.stopPropagation();
+    const rect = event.target.getBoundingClientRect();
+
+    actionPopup.style.top = rect.y + "px";
+    actionPopup.style.left = rect.x - 150 + "px";
+
+    actionPopup.style.opacity = "1";
+
+    setTimeout(() => {
+      window.addEventListener("click", handleOutsideClick);
+    }, 1);
+  });
+});
+
+function handleOutsideClick(event) {
+  if (event.target == actionPopup || actionPopup.contains(event.target)) return;
+
+  actionPopup.style.opacity = "0";
+  window.removeEventListener("click", handleOutsideClick, true);
+}
 
 function intiData() {
   browser.storage.sync.get([settingsIdentifier]).then((result) => {
@@ -45,6 +73,11 @@ function intiData() {
       (input) => {
         const key = input.getAttribute("eventId");
         input.checked = intialData[key];
+
+        const button = document.querySelector(`button[eventId="${key}"]`);
+        if (button) button.disabled = !intialData[key];
+
+        console.log(intialData, key, button);
       }
     );
   });
