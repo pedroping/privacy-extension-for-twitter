f672e7e08026be828d31a2125a86b2daf14ba55b
pedroping <pedro.dias@moneyprestador.com.br>
2025-08-11 19:19:15 -0300

    Feat: create trending topics blur

--- scripts/home-blur.js
+++ scripts/home-blur.js
@@ -1,6 +1,7 @@
-let lastParent;
+let lastPostsParent;
+let lastTrendingParent;
 
-function blueElements(elements) {
+function blurPostElements(elements) {
   Array.from(elements).forEach((post) => {
     if (
       post.classList.contains("blur-post") ||
@@ -27,17 +28,15 @@ function blueElements(elements) {
 
 function initScrollListner() {
   window.addEventListener("scroll", () => {
-    console.log("scroll");
-
-    if (lastParent && lastParent?.children?.length > 0) {
-      Array.from(lastParent.children).forEach((post) => {
+    if (lastPostsParent && lastPostsParent?.children?.length > 0) {
+      Array.from(lastPostsParent.children).forEach((post) => {
         post.classList.add("blur-post");
       });
     }
   });
 }
 
-async function homeBlur(data) {
+function postsBlur(data) {
   if (!data?.blurPost?.value) {
     document.body.style.setProperty("--post-blur-amount", "0px");
     document.body.style.setProperty("--post-blur-gray-scale", "0");
@@ -59,13 +58,79 @@ async function homeBlur(data) {
 
     if (!parent) return;
 
-    if (parent == lastParent) return;
+    if (parent == lastPostsParent) return;
 
-    lastParent = parent;
+    lastPostsParent = parent;
 
-    blueElements(parent.children);
+    blurPostElements(parent.children);
     observeDOM(parent, () => {
-      blueElements(parent.children);
+      blurPostElements(parent.children);
     });
   }, 200);
+}
+
+function blurTrendingElements(elements) {
+  Array.from(elements).forEach((post, i) => {
+    if (
+      post.classList.contains("blur-trending") ||
+      post.getAttribute("element-init") ||
+      i < 2
+    )
+      return;
+
+    post.classList.add("blur-trending");
+    post.setAttribute("element-init", true);
+
+    post.addEventListener("mouseenter", () => {
+      post.classList.remove("blur-trending");
+    });
+
+    post.addEventListener("mousemove", () => {
+      post.classList.remove("blur-trending");
+    });
+
+    post.addEventListener("mouseleave", () => {
+      post.classList.add("blur-trending");
+    });
+  });
+}
+
+// Trending blur
+
+function TrendingBlur(data) {
+  if (!data?.blurTrending?.value) {
+    document.body.style.setProperty("--trending-blur-amount", "0px");
+    document.body.style.setProperty("--trending-blur-gray-scale", "0");
+    return;
+  }
+
+  initScrollListner();
+
+  document.body.style.setProperty(
+    "--trending-blur-amount",
+    (data.blurTrending.blurAmount || 10) + "px"
+  );
+  document.body.style.setProperty("--trending-blur-gray-scale", "1");
+
+  setInterval(() => {
+    const parent = document.querySelector(
+      "#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div.css-175oi2r.r-aqfbo4.r-1l8l4mf.r-1hycxz > div > div.css-175oi2r.r-1hycxz.r-gtdqiz > div > div > div > div > div:nth-child(4) > section > div > div"
+    );
+
+    if (!parent) return;
+
+    if (parent == lastTrendingParent) return;
+
+    lastTrendingParent = parent;
+
+    blurTrendingElements(parent.children);
+    observeDOM(parent, () => {
+      blurTrendingElements(parent.children);
+    });
+  }, 200);
+}
+
+function homeBlur(data) {
+  postsBlur(data);
+  TrendingBlur(data);
 }
